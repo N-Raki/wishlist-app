b06884758c77b378fb1ffe91026fe5ad02f56072
Nathan Coustance <ncoustance@raki.app>
2024-03-03 23:23:31 +0100

    Removed Items from WishlistsList

--- Client/src/components/WishlistsList/WishlistsList.tsx
+++ Client/src/components/WishlistsList/WishlistsList.tsx
@@ -1,7 +1,7 @@
 import './WishlistsList.css'
 import {FC} from "react";
 import {Wishlist} from "../../models/wishlist.model.ts";
-import {Box, Link, Stack, Typography} from "@mui/material";
+import {Box, Link, Stack} from "@mui/material";
 
 interface WishlistsListProps {
     wishlists: Wishlist[];
@@ -13,20 +13,6 @@ const WishlistsList: FC<WishlistsListProps> = (props: WishlistsListProps) => {
                 {props.wishlists.map((wishlist) =>
                     <Box key={wishlist.id}>
                         <Link href={'/wishlists/' + wishlist.id} underline="hover">🔷 {wishlist.name}</Link>
-                        <ul>
-                            {wishlist.items.map((item) =>
-                                <li key={item.id}>
-                                    <Stack spacing={1} direction={'row'}>
-                                        {
-                                            item.url ?
-                                                <Link href={item.url} target="_blank">{item.name}</Link> :
-                                                <Typography>{item.name}</Typography>
-                                        }
-                                        {item.price ? <span> - {item.price}$</span> : null}
-                                    </Stack>
-                                </li>
-                            )}
-                        </ul>
                     </Box>
                 )}
         </Stack>
